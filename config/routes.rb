63a49e7ba2d1244aef6e8d53a5797613c4589086
Your Name <samanbatool08@gmail.com>
2020-07-29 00:40:23 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :games do 
-    resources :submitted_words
+  namespace :api do
+    namespace :v1 do
+      resources :games do 
+        resources :submitted_words
+      end
+    end
   end
 end
